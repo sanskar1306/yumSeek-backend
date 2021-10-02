5003ccb7d0b3229e0bc8fc2363463edfeff708a4
sanskar1306 <sanskarkabra1306@gmail.com>
2021-10-02 16:45:50 +0530

    mongodb connected

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once("open", function () {
+  console.log("MongoDB database connection established successfully");
+});
+
+app.listen(PORT, function () {
+  console.log("Server is running on Port: " + PORT);
+});
