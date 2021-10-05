e6472a8848324dc6737dddef78837def5f250cb0
sanskar1306 <sanskarkabra1306@gmail.com>
2021-10-05 14:09:41 +0530

    authentication done

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/users");
 require("dotenv").config();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL , { useNewUrlParser: true  });
 const connection = mongoose.connection;
 
 connection.once("open", function () {
   console.log("MongoDB database connection established successfully");
 });
+
+app.use("/api/users",authRoute);
 
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
