66513a37ef767a9483c0685db8659df9c5ae2f20
sanskar1306 <sanskarkabra1306@gmail.com>
2021-12-10 12:26:49 +0530

    res

--- controllers/restaurantProfile.controller.js
+++ controllers/restaurantProfile.controller.js
@@ -1,23 +1,29 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const auth = require("../middlewares/auth.js");
+const restaurant = require("../models/restaurant.model.js");
 const restaurantProfile = require("../models/restaurantProfile.model.js");
 const Joi = require("@hapi/joi");
 
 const getAllRestaurants = async (req, res) => {
-    try {
-        const restaurants = await restaurantProfile.find();
-        res.status(200).json(restaurants);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-
-}
+  try {
+    const restaurants = await restaurantProfile.find();
+    res.status(200).json(restaurants);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 const createProfile = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+  try {
+  } catch (error) {}
+};
+
+const updateProfile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const getRestaurantProfileById = async (req, res) => {};
+
+module.exports = { getAllRestaurants, createProfile, updateProfile };
--- routes/restaurantProfile.js
+++ routes/restaurantProfile.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 const restaurantProfileController = require("../controllers/restaurantProfile.controller.js");
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/uploads/restaurantProfile");
+    },
+    filename: function (req, file, cb) {
+        cb(
+          null,
+          new Date().getTime().toString() +
+            "-" +
+            file.fieldname +
+            path.extname(file.originalname)
+        );
+    }
+});
+
+const upload = multer({ dest: "uploads/" });
 router.get('/', restaurantProfileController.getAllRestaurantProfile);
 router.get('/restaurant/:id', restaurantProfileController.getRestaurantProfileById);
 router.post('/createProfile', restaurantProfileController.createRestaurantProfile);
